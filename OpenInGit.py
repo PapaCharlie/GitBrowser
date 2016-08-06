fae13a043e2967df5b6adbb1c8b30109b81a363d
Paul Chesnais <paul.chesnais@gmail.com>
2016-08-06 11:21:00 +0100

    Working prototype!
    Need to add BitBucket and GitLab support and add the ability to open
    from the Side Bar (for directories)

--- OpenInGit.py
+++ OpenInGit.py
@@ -2,8 +2,11 @@ import sublime, sublime_plugin
 import webbrowser, re, os, subprocess
 
 class OpenInGitCommand(sublime_plugin.WindowCommand):
+    def current_file(self):
+        return self.window.active_view().file_name()
+
     def pwd(self):
-        return os.path.sep.join(self.window.active_view().file_name().split(os.path.sep)[0:-1])
+        return os.path.sep.join(self.current_file().split(os.path.sep)[0:-1])
 
     # Returns the stdout, the stderr and the exit code
     def git_command(self, command):
@@ -20,13 +23,30 @@ class OpenInGitCommand(sublime_plugin.WindowCommand):
             branch = self.git_command("rev-parse --abbrev-ref HEAD")[0].rstrip()
             commit = self.git_command("rev-parse HEAD")[0].rstrip()
             root = self.git_command("rev-parse --show-toplevel")[0].rstrip()
-            if "github" in remote.lower():
-                
-            for region in self.window.active_view().sel():
-                if region[0] != region[1]: # Selection not empty
-                    firstline = self.window.active_view().rowcol(region.a)[0] + 1
-                    lastline = self.window.active_view().rowcol(region.b)[0] + 1
-                else:
-                    pass
+            path = re.sub(r"^/", "", self.current_file().replace(root, ""))
+
+            ref = commit if branch == "HEAD" else branch
+            if remote.startswith("git@"):
+                slug = re.search(r"^git@(.+)", remote).groups()[0].replace(":", "/")
+            else:
+                slug = remote
+
+            if "github" in slug.lower():
+                url = "https://{slug}/blob/{ref}/{path}".format(slug = slug, ref = ref, path = path)
+                print(ref)
+                print(url)
+                webbrowser.open(url)
+            else:
+                sublime.error_message(remote + " is not a supported git host!")
+            # for region in self.window.active_view().sel():
+            #     if region.a != region.b: # Selection not empty
+            #         firstline = self.window.active_view().rowcol(region.a)[0] + 1
+            #         lastline = self.window.active_view().rowcol(region.b)[0] + 1
+            #     else:
+            #         pass
         else:
             sublime.error_message("This is not inside a git directory!: " + err)
+
+class OpenInGitFromSidebarCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        pass
--- README.md
+++ README.md
@@ -1,24 +1,21 @@
-Wolfram|Alpha Lookup
+OpenInGit
 ========================
 
 Description
 ------------------
 
-This is a [Sublime Text 3](http://www.sublimetext.com/) plug-in that looks up the current selection on Wolfram|Alpha, and outputs the result on the line below.
-
-Setup
-------------------
-
-Please add this to your `Preferences.sublime-settings`:
-```
-"wolfram_api_key": "%API_KEY%"
-```
-HINT: You can access the file from the menu through `Preferences/Settings - User`.
-
-You can make and account and acquire an API key from the [Wolfram|Alpha Developer Portal](https://developer.wolframalpha.com/portal/).
+This is a [Sublime Text 3](http://www.sublimetext.com/) plug-in that attempts to open the given file or selection in the browser, in the correct domain.
 
 Shortcut Keys
 ------------------
 
 All Platforms:
-- Ctrl+Alt+a --> Lookup selection or current line.
+- Ctrl+Shift+o --> Open selection or current line.
+
+Side Bar
+------------------
+
+Open the file or directory directly.
+
+Currently supported Git repos hosts:
+  Github.com
